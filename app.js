962714046c9b8dfc21fb43c9ca6874a445009a1b
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-03-15 21:39:20 -0700

    completed

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 
 
-let sum = function(a, b, c, d) {
+let sum = (a, b, c, d) => {
   return a + b + c + d;
 };
 
@@ -116,7 +116,7 @@ let sum = function(a, b, c, d) {
 console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
+let objectLit = () => {
   return {
     key1: 'value1',
     key2: 'value2',
@@ -128,7 +128,7 @@ let objectLit = function() {
 console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
+let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
@@ -138,7 +138,7 @@ let sumAndProduct = function(a, b) {
 console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
+let message = (name) => {
   return `Hello, ${name}!`;
 };
 
@@ -146,7 +146,7 @@ let message = function(name) {
 console.log(message('Allie'));
 
 
-let Student = function(name, age, hometown) {
+let Student = function (name, age, hometown) {
   this.name = name;
   this.age = age;
   this.hometown = hometown;
@@ -162,7 +162,7 @@ console.log(joe);
 
 
 
-Student.prototype.greeting = function() {
+Student.prototype.greeting = () => {
   return `Hi, my name is ${this.name}`;
 };
 
@@ -174,7 +174,7 @@ console.log(joe.greeting());
 
 
 
-Student.courseName = function() {
+Student.courseName = () => {
   return 'This student is enrolled in Code 301.';
 };
 
